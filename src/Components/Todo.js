0d46a626b5ffbb3219a46f4ace8f6876edcd5ccb
amyrasl <65806232+amyrasl@users.noreply.github.com>
2023-12-21 12:24:18 +0700

    3RD COMMITT
    Edit func done~

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,19 +1,82 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import { IoSquareOutline } from "react-icons/io5";
 import { IoCheckboxOutline } from "react-icons/io5";
+import { IconContext } from "react-icons";
 
-export const Todo = ({ task, completedTodo, removeTodo }) => {
+export const Todo = ({ task, completedTodo, removeTodo, updateTodo }) => {
+  const [text, setText] = useState(task.task);
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    textareaRef.current.style.height = "0px";
+    const scrollHeight = textareaRef.current.scrollHeight;
+    textareaRef.current.style.height = scrollHeight + "px";
+  }, [text]);
+
+  const handleBlur = (event) => {
+    event.preventDefault();
+    updateTodo(text, task.id);
+  };
+
   return (
     <div className="todo-row">
       {task.completed ? (
-        <IoCheckboxOutline onClick={() => completedTodo(task.id)} />
+        <>
+          <IconContext.Provider
+            value={{
+              color: "#686866",
+              size: "1.75rem",
+            }}
+          >
+            <IoCheckboxOutline onClick={() => completedTodo(task.id)} />
+          </IconContext.Provider>
+          <span className="todo-content">
+            <textarea
+              rows={1}
+              disabled={true}
+              ref={textareaRef}
+              value={text}
+              className="textclass completed"
+            >
+              {task.task}
+            </textarea>
+          </span>
+        </>
       ) : (
-        <IoSquareOutline onClick={() => completedTodo(task.id)} />
+        <>
+          <IconContext.Provider
+            value={{
+              color: "#686866",
+              size: "1.75rem",
+            }}
+          >
+            <IoSquareOutline onClick={() => completedTodo(task.id)} />
+          </IconContext.Provider>
+          <span className="todo-content">
+            <form id="myForm">
+              <textarea
+                ref={textareaRef}
+                value={text}
+                className="textclass"
+                onChange={(e) => setText(e.target.value)}
+                onBlur={(e) => handleBlur(e)}
+              >
+                {task.task}
+              </textarea>
+            </form>
+          </span>
+        </>
       )}
 
-      <p className="todo-content">{task.task}</p>
-      <IoCloseCircleOutline onClick={() => removeTodo(task.id)} />
+      <IconContext.Provider
+        value={{
+          color: "#FF8A8A",
+          size: "1.2rem",
+        }}
+      >
+        <IoCloseCircleOutline onClick={() => removeTodo(task.id)} />
+      </IconContext.Provider>
     </div>
   );
 };
--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TodoForm } from "./TodoForm";
 import { Todo } from "./Todo";
 import { v4 as uuidv4 } from "uuid";
@@ -30,9 +30,18 @@ export const TodoWrapper = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const updateTodo = (task, id) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, task } : todo)));
+  };
+
+  useEffect(() => {
+    // This effect runs after the component re-renders
+    console.log("Updated Todos:", todos);
+  }, [todos]); // Dependency array ensures the effect runs only when 'todos' changes
+
   return (
     <div className="todowrapper">
-      <h1>What is your plan today?</h1>
+      <h1>Tell me your plans!</h1>
       {todos.length === 0 && (
         <img
           className="imageclass"
@@ -49,6 +58,7 @@ export const TodoWrapper = () => {
             key={todo.id}
             completedTodo={completedTodo}
             removeTodo={removeTodo}
+            updateTodo={updateTodo}
           />
         ))}
       </div>
